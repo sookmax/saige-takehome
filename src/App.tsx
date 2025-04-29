d542af3715c4a540583b11ba5f2c2f8442c621b2
Sook <sukkyu.chung@gmail.com>
2025-04-29 11:35:44 +0900

    set up shadcn/ui & tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import './App.css'
-
 function App() {
-
-  return (
-    <div>
-      이곳에 To-Do App을 작성해주세요.
-    </div>
-  )
+  return <div>이곳에 To-Do App을 작성해주세요.</div>
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  // shadcn/ui
+  // https://ui.shadcn.com/docs/installation/vite#update-viteconfigts
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
