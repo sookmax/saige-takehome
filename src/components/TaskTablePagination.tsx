dffb51cba4bd2aaa02a1dbf535e392d0ba29574f
Sook <sukkyu.chung@gmail.com>
2025-05-02 17:05:22 +0900

    add page row indices information in TaskTablePagination

--- src/components/TaskTablePagination.tsx
+++ src/components/TaskTablePagination.tsx
@@ -6,7 +6,7 @@ import { getPageIndices } from '@/lib/pagination'
 export function TaskTablePagination({ table }: { table: Table<ToDo> }) {
   const { pagination } = table.getState()
 
-  const { pageIndex: currentPageIndex } = pagination
+  const { pageIndex: currentPageIndex, pageSize } = pagination
   const pageIndices = getPageIndices(
     0,
     table.getPageCount() - 1,
@@ -14,23 +14,34 @@ export function TaskTablePagination({ table }: { table: Table<ToDo> }) {
     5
   )
 
+  const totalRowCount = table.getRowCount()
+  const firstRowIndex = currentPageIndex * pageSize
+  const lastRowIndex = Math.min(firstRowIndex + pageSize - 1, totalRowCount - 1)
+
   return (
-    <div className="space-x-1 flex justify-center">
-      {pageIndices.map((pageIndex, index) => {
-        return pageIndex === '...' ? (
-          <span key={`ellipsis-${index}`}>...</span>
-        ) : (
-          <Button
-            key={`page-${pageIndex}`}
-            variant={pageIndex === currentPageIndex ? 'default' : 'outline'}
-            size="icon"
-            onClick={() => table.setPageIndex(pageIndex)}
-            className="h-8 w-8 transition-none"
-          >
-            {pageIndex + 1}
-          </Button>
-        )
-      })}
+    <div className="flex items-center justify-between">
+      <div className="text-sm text-muted-foreground">
+        {`Showing ${firstRowIndex + 1} to ${
+          lastRowIndex + 1
+        } of ${totalRowCount} results`}
+      </div>
+      <div className="space-x-1 flex justify-center">
+        {pageIndices.map((pageIndex, index) => {
+          return pageIndex === '...' ? (
+            <span key={`ellipsis-${index}`}>...</span>
+          ) : (
+            <Button
+              key={`page-${pageIndex}`}
+              variant={pageIndex === currentPageIndex ? 'default' : 'outline'}
+              size="icon"
+              onClick={() => table.setPageIndex(pageIndex)}
+              className="h-8 w-8 transition-none"
+            >
+              {pageIndex + 1}
+            </Button>
+          )
+        })}
+      </div>
     </div>
   )
 }
